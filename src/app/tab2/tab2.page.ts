0ecf081d122cdd4b377bff00f2e2a5e15b1739c4
Evan <iproto@gmail.com>
2019-07-16 23:11:56 -0600

    Initial project creation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Plugins, Capacitor } from '@capacitor/core';
+import { YoutubePlayerWeb } from 'capacitor-youtube-player';
 
 @Component({
   selector: 'app-tab2',
@@ -9,4 +11,16 @@ export class Tab2Page {
 
   constructor() {}
 
+  async testNativeYoutubePlayerPlugin() {
+
+    const { YoutubePlayer } = Plugins;
+
+    const options = {width: 640, height: 360, videoId: 'tDW2C6rcH6M'};
+    const playerReady = await YoutubePlayer.initialize(options);
+  }
+
+  async testWebYoutubePlayerPlugin() {
+      const options = {playerId: 'youtube-player', playerSize: {width: 640, height: 360}, videoId: 'tDW2C6rcH6M'};
+      const result = await YoutubePlayerWeb.initialize(options);
+  }
 }
